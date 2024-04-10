5f1f823066b2eb469d6e19f04432043a1f615748
kg <kylegoslin@gmail.com>
2024-04-10 15:17:31 +0100

    first commit

--- visual_calendar_extension/scripts/content.js
+++
@@ -1,8 +0,0 @@
-const article = document.querySelector("article");
-
-
-let currentURL = window.location;
-
-alert("current address is: " + currentURL);
-
-fetch('http://localhost:5000?xxxxxxxxxxxxxxxxxxxxxxxxxxx')
